be510a646baeb780bbfde03960ee91a0de04c83d
Anil Gupta <anil.g@zebpay.com>
2021-06-06 12:14:52 +0530

    Project setup

---
+++ TrisysDemo/TrisysDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrisysDemo
+//
+//  Created by Anil Gupta on 06/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
